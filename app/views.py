5aebaff97700ec0715c7ca7245d4ce68320ce613
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-22 09:02:19 +0200

    API fetch functionality

--- app/views.py
+++ app/views.py
@@ -14,6 +14,32 @@ def index(request):
 
     if request.method == 'POST':
         city1 = request.POST['city1']
-        city2 = request.POST['city2']
+        city2 = request.get('city2', None)
     else:
         return render(request, 'w_app/index.html')
+    
+def fetch_weather_and_forecast(city, api_key, cw_url, fc_url):
+    response = requests.get(cw_url.format(city, api_key)).json()
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+    forecast_response = requests.get(fc_url.format(lat, lon, api_key)).json()
+
+    weather_data = {
+        'city': city,
+        'temperature': round(response['main']['temp'] - 273.15, 2),
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon']
+    }
+
+    daily_forecasts = []
+
+    for daily_data in forecast_response['daily'][:5]:
+        daily_forecasts.append({
+            'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+            'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
+            'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
+            'description': daily_data['weather'][0]['description'],
+            'icon': daily_data['weather'][0]['icon']
+
+        })
+
+
